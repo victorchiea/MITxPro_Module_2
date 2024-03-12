eaed28a64fe525352deaafe38e63b1a16ec3f43d
victorchiea <v.chiea@gmail.com>
2024-03-11 22:15:28 -0300

    Commit 5

---
+++ part2_ml_using python.py
@@ -0,0 +1,6 @@
+# Section of the Module for implementing Python on Machine Learning
+# The implementation will be with Regresssion and Classification models consisting of a single neuron
+# No libraries or classes will be used in this part
+print("Implementing Regression With a Single Neuron Model")
+print()
+import numpy as np
